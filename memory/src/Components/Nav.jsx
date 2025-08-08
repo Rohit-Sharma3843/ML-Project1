fe1f7a59445a4aa14ae546859473ea8e37cb1894
Rohit-Sharma3843 <157728855+Rohit-Sharma3843@users.noreply.github.com>
2025-08-09 00:03:20 +0530

    Update Nav.jsx

--- memory/src/Components/Nav.jsx
+++ memory/src/Components/Nav.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import img1 from "../../images/brain.png";
-import img2 from "../../images/play.png";
-import img3 from "../../images/info.png";
+import img1 from "/images/brain.png";
+import img2 from "/images/play.png";
+import img3 from "images/info.png";
 import { NavLink } from "react-router-dom";
-import img4 from "../../images/night.png";
-import img5 from "../../images/light.png";
+import img4 from "/images/night.png";
+import img5 from "/images/light.png";
 const Nav = (props) => {
   const [mode, alter] = props.variables;
   return (
