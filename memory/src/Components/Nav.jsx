5e8e88b701db941bac32a49ca0370c14a1322b7d
Rohit-Sharma3843 <157728855+Rohit-Sharma3843@users.noreply.github.com>
2025-08-09 16:27:24 +0530

    Update Nav.jsx

--- memory/src/Components/Nav.jsx
+++ memory/src/Components/Nav.jsx
@@ -8,51 +8,55 @@ import img5 from "/images/light.png";
 const Nav = (props) => {
   const [mode, alter] = props.variables;
   return (
-    <div className="w-[100vw] p-2 flex justify-evenly items-center bg-gradient-to-r from-purple-900 via-blue-900 to-indigo-900">
-      <div className="flex justify-center items-center gap-3">
-        <div className="p-1 bg-white rounded-2xl hover:rotate-y-180 transition-all ease-in-out duration-400">
-          <img className="h-[70px]" src={img1} alt="" />
-        </div>
-        <div className="flex flex-col gap-1 text-white">
-          <h3
-            className="font-bold text-2xl bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent
+    <div className=" outside w-[100vw] p-2 flex justify-evenly items-center bg-gradient-to-r from-purple-900 via-blue-900 to-indigo-900">
+      <div>
+        <div className="flex justify-center items-center gap-3">
+          <div className="p-1 bg-white rounded-2xl hover:rotate-y-180 transition-all ease-in-out duration-400">
+            <img className="h-[70px]" src={img1} alt="" />
+          </div>
+          <div className="flex flex-col gap-1 text-white">
+            <h3
+              className="font-bold text-2xl bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent
 "
-          >
-            Memory Master
-          </h3>
-          <span>Flip and Match</span>
+            >
+              Memory Master
+            </h3>
+            <span>Flip and Match</span>
+          </div>
         </div>
       </div>
-      <NavLink to="/" className="group">
-        <div className="flex flex-col items-center justify-center gap-3 text-white">
-          <div className="p-1 m-0 rounded-full group-hover:scale-105 transition-all duration-150 bg-white">
-            <img className="w-[50px]" src={img3} alt="" />
+      <div className="w-[60vw] buttons flex items-center justify-evenly">
+        <NavLink to="/" className="group">
+          <div className="flex flex-col items-center justify-center gap-3 text-white">
+            <div className="p-1 m-0 rounded-full group-hover:scale-105 transition-all duration-150 bg-white">
+              <img className="w-[50px]" src={img3} alt="" />
+            </div>
+            <p className=" w-[100px] text-center group-hover:font-bold box-border transition-all duration-150">
+              About Game
+            </p>
           </div>
-          <p className=" w-[100px] text-center group-hover:font-bold box-border transition-all duration-150">
-            About Game
+        </NavLink>
+        <NavLink to="/theme" className="group">
+          <div className="flex flex-col items-center justify-center gap-3 text-white">
+            <div className="p-1 m-0 rounded-full group-hover:scale-105 transition-all duration-150 bg-white">
+              <img className="w-[50px]" src={img2} alt="" />
+            </div>
+            <p className=" w-[100px] text-center group-hover:font-bold box-border transition-all duration-150">
+              Start Game
+            </p>
+          </div>
+        </NavLink>
+        <div
+          onClick={alter}
+          className="flex box-border w-[200px] text-white group flex-col items-center justify-center"
+        >
+          <div className="box-border w-[60px] p-1  m-0 rounded-full group-hover:scale-105 transition-all duration-150 bg-white">
+            <img src={mode == "dark" ? img4 : img5} alt="" />
+          </div>
+          <p className="text-center group-hover:font-bold box-border transition-all duration-150">
+            Switch to {mode} mode
           </p>
         </div>
-      </NavLink>
-      <NavLink to="/theme" className="group">
-        <div className="flex flex-col items-center justify-center gap-3 text-white">
-          <div className="p-1 m-0 rounded-full group-hover:scale-105 transition-all duration-150 bg-white">
-            <img className="w-[50px]" src={img2} alt="" />
-          </div>
-          <p className=" w-[100px] text-center group-hover:font-bold box-border transition-all duration-150">
-            Start Game
-          </p>
-        </div>
-      </NavLink>
-      <div
-        onClick={alter}
-        className="flex box-border w-[200px] text-white group flex-col items-center justify-center"
-      >
-        <div className="box-border w-[60px] p-1  m-0 rounded-full group-hover:scale-105 transition-all duration-150 bg-white">
-          <img src={mode == "dark" ? img4 : img5} alt="" />
-        </div>
-        <p className="text-center group-hover:font-bold box-border transition-all duration-150">
-          Switch to {mode} mode
-        </p>
       </div>
     </div>
   );
